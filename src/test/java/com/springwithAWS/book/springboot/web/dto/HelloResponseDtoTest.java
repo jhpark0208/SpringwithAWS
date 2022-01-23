d9e52dc31ab157bb9eef2fc27ea963953e5caf79
jhpark0208 <jeonghoonpark.dev@gmail.com>
2022-01-23 17:03:29 +0900

    Chapter 2

---
+++ src/test/java/com/springwithAWS/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,18 @@
+package com.springwithAWS.book.springboot.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void LombokTest(){
+        String name = "test";
+        int amount = 100;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
